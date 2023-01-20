c2daf4d7c77f882d0688790fbe9e70b3516c0c59
Wojciech Gola <91955404+wgola@users.noreply.github.com>
2023-01-20 18:52:10 +0000

    added missing routes and services

--- app.py
+++ app.py
@@ -87,5 +87,65 @@ def delete_employee(id):
     return jsonify({"message": "Coulnd't delete employee"}), 500
 
 
+@app.route("/employees/<int:id>/subordinates", methods=['GET'])
+def get_all_subordinates(id):
+    with DRIVER.session() as session:
+        if_manager = session.execute_read(app_services.check_if_manager, id)
+
+        if if_manager:
+            subordinates = session.execute_read(
+                app_services.get_all_subordinates, id)
+
+            if subordinates:
+                return jsonify({"subordinates": subordinates}), 200
+
+            return jsonify({"message": "There are no subordinates of this manager"}), 404
+
+        return jsonify({"message": "This employee isn't manager"}), 404
+
+
+@app.route("/employees/<int:id>/department", methods=['GET'])
+def get_department_info(id):
+    with DRIVER.session() as session:
+        department_info = session.execute_read(
+            app_services.get_department_info, id)
+
+    return jsonify({"department": department_info}), 200
+
+
+@app.route("/departments", methods=['GET'])
+def get_departments():
+    args = request.args
+    with DRIVER.session() as session:
+        departments = session.execute_read(
+            app_services.get_all_departments, args)
+
+    return jsonify({"departments": departments}), 200
+
+
+@app.route("/departments/<int:id>", methods=['GET'])
+def get_department_by_id(id):
+    with DRIVER.session() as session:
+        department = session.execute_read(
+            app_services.get_department_by_id, id)
+
+    if department:
+        return jsonify({"department": department})
+
+    return jsonify({"message": "There isn't department with this ID"}), 404
+
+
+@app.route("/departments/<int:id>/employees")
+def get_all_employees_of_department(id):
+    with DRIVER.session() as session:
+        employees = session.execute_read(
+            app_services.get_all_employees_of_department, id)
+
+    if employees:
+        return jsonify({"employees": employees}), 200
+
+    return jsonify({"message": "Couldn't find any employees"})
+
+
 if __name__ == '__main__':
     app.run()
--- app_services.py
+++ app_services.py
@@ -26,7 +26,7 @@ class app_services():
         if queries:
             match_query = "WHERE {}".format(queries[0])
             for i in range(1, len(queries)):
-                match_query += ", {}".format(queries[i])
+                match_query += "AND {}".format(queries[i])
 
         if match_query:
             final_query = "MATCH (m:Employee) {} RETURN m, ID(m)".format(
@@ -70,7 +70,7 @@ class app_services():
         if type == "manager":
             relationship = "-[r:MANAGES]->"
         else:
-            relationship = "-[r:WORKS_INT]->"
+            relationship = "-[r:WORKS_IN]->"
 
         query = "MATCH (m:Department WHERE m.name = '{}') CREATE (n:Employee {}){}(m)".format(
             department, employee_data, relationship)
@@ -193,3 +193,86 @@ class app_services():
             return True
 
         return False
+
+    @staticmethod
+    def check_if_manager(tx, id):
+        relationship_query = "MATCH (m:Employee)-[r]->(n) WHERE ID(m) = {} RETURN type(r)".format(
+            id)
+        result = tx.run(relationship_query).data()
+
+        if result:
+            return result[0]['type(r)'] == "MANAGES"
+
+        return False
+
+    @staticmethod
+    def get_all_subordinates(tx, id):
+        get_subordinates_query = "MATCH (m:Employee)-[:MANAGES]->(n)<-[:WORKS_IN]-(g) WHERE ID(m) = {} RETURN g, ID(g)".format(
+            id)
+        results = tx.run(get_subordinates_query).data()
+
+        return [{"id": result['ID(g)'], **result["g"]} for result in results]
+
+    @staticmethod
+    def get_department_info(tx, employee_id):
+        get_department_query = "MATCH (m:Employee)-[]->(n:Department)<-[r]-() WHERE ID(m) = {} RETURN n, COUNT(r)".format(
+            employee_id)
+        result = tx.run(get_department_query).data()
+
+        return {**result[0]['n'], "number_of_workers": result[0]['COUNT(r)'] + 1}
+
+    @staticmethod
+    def get_all_departments(tx, args):
+        name = args.get("name")
+        workers_count = args.get("workers_count")
+        sort_by = args.get("sort_by")
+        order = args.get("order", default="asc")
+
+        final_query = "MATCH (n:Department)<-[r]-() WITH COUNT(r) as count, n RETURN count, n, ID(n)"
+
+        queries = []
+        if name:
+            name_query = "n.name = '{}'".format(name)
+            queries.append(name_query)
+
+        if workers_count:
+            count_query = "COUNT(r) = {}".format(workers_count)
+            queries.append(count_query)
+
+        match_query = ""
+        if queries:
+            match_query = "WHERE {}".format(queries[0])
+            for i in range(1, len(queries)):
+                match += "AND {}".format(queries[i])
+
+        if match_query:
+            final_query = "MATCH (n:Department)<-[r]-() WITH COUNT(r) as count, n {} RETURN count, n, ID(n)".format(
+                match_query)
+
+        if sort_by:
+            final_query += "ORDER BY n.{}".format(
+                sort_by.strip(' " " ') + " " + order.upper())
+
+        results = tx.run(final_query).data()
+
+        return [{"id": result['ID(n)'], **result['n'], "workers_count": result['count']} for result in results]
+
+    @staticmethod
+    def get_department_by_id(tx, id):
+        department_query = "MATCH (n:Department)<-[r]-() WITH COUNT(r) as count, n WHERE ID(n) = {} RETURN count, n, ID(n)".format(id)
+        result = tx.run(department_query).data()
+
+        if result:
+            return {"id": result[0]['ID(n)'], **result[0]['n'], "workers_count": result[0]['count']}
+
+        return {}
+
+    @staticmethod
+    def get_all_employees_of_department(tx, id):
+        get_employees_query = "MATCH (n:Department)<-[]-(m:Employee) WHERE ID(n) = {} RETURN m, ID(m)".format(id)
+        results = tx.run(get_employees_query).data()
+
+        if results:
+            return [{"id": result['ID(m)'], **result['m']} for result in results]
+
+        return []
